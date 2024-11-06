f20a0fa6fdfd4a9d4a064e91d45ecea4106f3abf
Shanese Archibald <shanese.archibald@gmail.com>
2024-11-06 15:24:05 -0400

    Edited scripts.js and styles.css

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 /* Add your JavaScript to this file */
+
+function subscribe() {
+    const email = document.getElementById('email').value;
+    const message = document.querySelector('.message');
+    if (email) {
+        message.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+    } else {
+        message.textContent = 'Please enter a valid email address.';
+    }
+}
